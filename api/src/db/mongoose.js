c6392dba31786facb18929f8d15046f3a4bf0b38
Juan Maria Migliore <juanmamig@Juans-MacBook-Pro.local>
2021-08-02 14:32:53 -0300

    first commit

--- api/src/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const uri = "mongodb+srv://juanmamig:mnPfYtqOX2NYMRkM@cluster0.xt2ut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
